49705bc2f55017353bff606dd091b135ba01ffed
Nicolas Surian <nicolas.surian@gmail.com>
2018-08-29 07:41:05 +0200

    SF_CV / 1.2
    Finish template

--- src/Com/Nairus/ResumeBundle/Controller/PublicController.php
+++ src/Com/Nairus/ResumeBundle/Controller/PublicController.php
@@ -4,19 +4,34 @@ namespace Com\Nairus\ResumeBundle\Controller;
 
 use Com\Nairus\ResumeBundle\NSResumeBundle;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class PublicController extends Controller {
 
-    const NAME = "Public";
+    private const NAME = NSResumeBundle::NAME . ':' . "Public";
 
     /**
      * Index controller.
      *
      * @return Response
      */
-    public function indexAction(): Response {
-        return $this->render(NSResumeBundle::NAME . ':' . self::NAME . ':index.html.twig');
+    public function indexAction(Request $request, $page): Response {
+        // Bug chrome
+        if ("" === $page) {
+            $page = 1;
+        }
+
+        $datesFormat = $this->container->getParameter("dates_format");
+        $maxCardPerPage = $this->container->getParameter("max-cards-per-page");
+
+        return $this->render(self::NAME . ':index.html.twig', [
+                    "currentPage" => $page,
+                    "pages" => 10,
+                    "today" => new \DateTimeImmutable(),
+                    "dateFormat" => $datesFormat[$request->getLocale()],
+                    "maxCardPerPage" => $maxCardPerPage
+        ]);
     }
 
 }
